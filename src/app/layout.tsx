e495023918728f8e81b46c41150b863cf714c6e2
Off-Grid-Dev <skinnykdev@gmail.com>
2026-06-12 04:40:33 +0200

    initial setup and theme added to globals.css

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+const jetbrains = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Okka Elektro",
+  description: "Elektro reperasjon og salg",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} ${jetbrains.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
